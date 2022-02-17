4202fe2488ab0cdb220e9c0ad359b82df4c126a0
Nghi Le <vinhnghi223@gmail.com>
2022-02-17 20:25:18 +0200

    fixed projects thumbnails

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3000);
+connect().use(serveStatic(__dirname + '/docs')).listen(3000);
+
 console.log('Server running at http://localhost:3000/docs/#/about-me');
