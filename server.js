1dcca015fcbd5c9feb16e2b02d901908472dee03
Nghi Le <nghi.le@varian.com>
2025-07-03 11:58:08 +0300

    adjusted url

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/docs')).listen(3000);
 
-console.log('Server running at http://localhost:3000/docs/#/about-me');
+console.log('Server running at http://localhost:3000/#/about-me');
